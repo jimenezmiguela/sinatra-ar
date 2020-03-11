b366b811a5a585f656e2d47c4f7a8e9b779ebd9b
Ubuntu <ubuntu@ip-172-31-92-24.ec2.internal>
2020-03-11 01:49:11 +0000

    Active Record

---
+++ db/migrate/20200310173505_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+        t.string :email
+        t.string :name
+        t.timestamps
+        # t.type :column_name
+      end
+  end
+end
